4dd0102870df575d2e4e1041d225c61d4cea12ca
Sara Rojas Martínez <saritarojas0613@gmail.com>
2024-08-22 11:03:31 -0500

    add_tag et mark_completed

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,1 +1,25 @@
 # TODO: Add code here
+
+class Todo:
+
+    def __init__(self, code_id, title, description):
+        self.code_id: int = code_id
+        self.title: str = title
+        self.description: str = description
+        self.completed: bool = False
+        self.tags: (list[str]) = []
+
+    def mark_completed(self):
+        self.completed = True
+
+    def add_tag(self,tag: str):
+        if tag not in  self.tags:
+            self.tags.append(tag)
+    def __str__(self)->str:
+        return "code_id - title"
+
+
+class TodoBook:
+
+
+
